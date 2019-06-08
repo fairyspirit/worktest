178fd4abc6b41a8c483afdc8eebd9cbd44ffd452
fairyspirit <kseniyabenderska@gmail.com>
2019-06-09 01:35:29 +0300

    owl carousel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+$(document).ready(function(){
+	$(function() {
+	    $('.slider1').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        margin:30,
+				nav:true,
+				navText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
         autoplay:true,
         autoplayTimeout:3000,
         autoplayHoverPause:true,
@@ -19,6 +21,60 @@ $(document).ready(function(){
             }
         }
 		});
+		$('.slider2').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			autoplay:true,
+			autoplayTimeout:3000,
+			autoplayHoverPause:true,
+			responsive:{
+					0:{
+							items:1
+					},
+					600:{
+							items:2
+					}
+			}
+	}
+	);	
+});
+$(function() {
+	var owl = $('.slider2'),
+			owlOptions = {
+					loop: false,
+					margin: 10,
+					responsive: {
+							0: {
+									items: 1
+							},
+							780: {
+								items: 2
+							}
+					}
+			};
+
+	if ( $(window).width() < 854 ) {
+			var owlActive = owl.owlCarousel(owlOptions);
+	} else {
+			owl.addClass('off');
+	}
+
+	$(window).resize(function() {
+			if ( $(window).width() < 854 ) {
+					if ( $('.slider2').hasClass('off') ) {
+							var owlActive = owl.owlCarousel(owlOptions);
+							owl.removeClass('off');
+					}
+			} else {
+					if ( !$('.slider2').hasClass('off') ) {
+							owl.addClass('off').trigger('destroy.owl.carousel');
+							owl.find('.owl-stage-outer').children(':eq(0)').unwrap();
+					}
+			}
+	});
+});	
+		
 		$(function(){
 		$('.menu__icon').on('click', function(){
 
